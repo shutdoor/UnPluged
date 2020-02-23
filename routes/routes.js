ff0830ced9a779067fe6b099d7546b8a1938207c
DEUSYEET <47902658+DEUSYEET@users.noreply.github.com>
2020-02-23 12:07:23 -0700

    db testing

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-// mongoose.connect("mongodb+srv://JavaScriptMain:5LBF$7=pVaVt3ta@unplugged-a8oex.azure.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true , useUnifiedTopology: true }).catch(error=> console.log("error"));
-mongoose.connect("mongodb://localhost:27017/Test", {
-    useNewUrlParser: true
-}).catch(error => console.log("error"));
+mongoose.connect("mongodb+srv://tester:a@unplugged-a8oex.azure.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true , useUnifiedTopology: true }).catch(error=> console.log("error"));
+// mongoose.connect("mongodb://localhost:27017/Test", {useNewUrlParser: true}).catch(error => console.log("error"));
 
 var mdb = mongoose.connection;
 mdb.on('error', console.error.bind(console, 'connection error:'));
@@ -28,7 +26,7 @@ const config = require('../config')
 exports.index = (req, res) => {
     // post.create({
     //     time: Date.now().toString(),
-    //     user: "Test",
+    //     user: "TestUser",
     //     content: "Test Content",
     //     score: 10
     // },(err, test)=>{

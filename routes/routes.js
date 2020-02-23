fca13ababcfdbe38e043562e2dd1ca91f72b2306
DEUSYEET <47902658+DEUSYEET@users.noreply.github.com>
2020-02-23 13:17:42 -0700

    Create posts and comments

--- routes/routes.js
+++ routes/routes.js
@@ -16,6 +16,13 @@ var PostSchema = new mongoose.Schema({
     content: String,
     score: Number
 });
+var CommentSchema = new mongoose.Schema({
+    time: String,
+    user: String,
+    content: String,
+    score: Number,
+    contextPostID :String
+});
 
 var accountSchema = mongoose.Schema({
     username: String,
@@ -24,6 +31,7 @@ var accountSchema = mongoose.Schema({
 });
 
 var post = mongoose.model("Post_Collection", PostSchema)
+var comment = mongoose.model("Comment_Collection", PostSchema)
 var Account = mongoose.model('Account_Collection', accountSchema);
 
 
@@ -70,12 +78,17 @@ exports.main = (req, res) => {
 
 
 exports.vote = (req, res)=>{
-    console.log(req.body);
+    // console.log(req.body);
     var score = req.body.currentScore
-    if(req.body.Vote == "Up"){
-        score++;
+    if (req.body.Vote == "comment"){
+        var postID = encodeURIComponent(req.body.id)
+        res.redirect('/comment' +"/?postID="+ postID);
     } else if (req.body.Vote == "Down"){
         score--;
+        res.redirect('/feed')
+    } else if(req.body.Vote == "Up"){
+        score++;
+        res.redirect('/feed')
     }
     post.findByIdAndUpdate(req.body.id, {
         $set:{
@@ -85,7 +98,6 @@ exports.vote = (req, res)=>{
         if (err) throw err;
     });
 
-    res.redirect('/')
 }
 
 exports.signUp = (req, res) => { //signing up
@@ -94,3 +106,57 @@ exports.signUp = (req, res) => { //signing up
         "config": config
     })
 };
+
+exports.makePost = (req, res)=>{
+    res.render('post', {
+        "title": 'Make Post'
+    } )
+}
+
+exports.createPost = (req,res)=>{
+    post.create({
+        time: Date.now().toString(),
+        user: "TestUser",
+        content: req.body.postText,
+        score: 0
+    },(err, test)=>{
+        if (err) return console.error(err);
+        console.log(test.toString() + " Added");
+    });
+
+
+
+
+    res.redirect('/feed');
+}
+
+exports.comment= (req,res)=>{
+    var contextPostID =  req.query.postID;
+    var contextPost = post.findById(contextPostID, (err,cPost)=>{
+        if (err) return console.error(err);
+        res.render('comment', {
+            "title":"Comment",
+            "postContent":cPost.content
+        })
+    })
+}
+
+exports.createComment = (req,res)=>{
+    var contextPostID =  req.query.postID;
+    var contextPost = post.findById(contextPostID, (err,cPost)=>{
+        if (err) return console.error(err)
+        comment.create({
+            time: Date.now().toString(),
+            user: "TestUser",
+            content: req.body.postText,
+            score: 0,
+            contextPostID: contextPostID
+        },(err, test)=>{
+            if (err) return console.error(err);
+            console.log(test.toString() + " Added");
+        });
+
+    })
+
+    res.redirect('/feed');
+}
